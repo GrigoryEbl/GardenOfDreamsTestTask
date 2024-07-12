bb63c4531460f1df406b8831c3b6b71d93a172d0
GrigoryEbl <yatrahal.rus@mail.ru>
2024-07-12 15:30:21 +0400

    Add flipping player model

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,22 +7,42 @@ public class Mover : MonoBehaviour
     [SerializeField] private float _speed;
 
     private Transform _transform;
+    private Rigidbody2D _rigidbody2D;
 
     public bool IsMoving { get; private set; }
 
     private void Awake()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         _transform = transform;
     }
 
     public void Move(Vector2 direction)
     {
-        _transform.Translate(direction * _speed);
+        _rigidbody2D.velocity = direction * _speed;
+
+        Flip();
+
         IsMoving = true;
     }
 
     public void Stop()
     {
+        if (_rigidbody2D != null)
+            _rigidbody2D.velocity = Vector3.zero;
+
         IsMoving = false;
+    }
+
+    private void Flip()
+    {
+        if (_rigidbody2D.velocity.x < 0)
+        {
+            _transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (_rigidbody2D.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
